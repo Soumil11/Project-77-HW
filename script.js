3b0e56dc77c0198920bafb3396fb1b586c400617
Soumil11 <93382280+Soumil11@users.noreply.github.com>
2021-10-29 17:01:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+console.log("My first console project");
+
+var firstName = "Soumil";
+console.log(firstName);
+
+var lastName = "Bedi";
+console.log(lastName);
+
+var fullName = firstName.concat(lastName);
+console.log(fullName);
+
+var add = A + B;
+console.log(add);
+
+var squareRoot = Math.sqrt(25);
+console.log(squareRoot);
